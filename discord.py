b1b81d8848c4826c1fa65596f7bb8b82f47037b5
AwerBow <awersmanser@gmail.com>
2023-07-08 01:30:02 +0300

    first commit

---
+++ discord.py
@@ -0,0 +1,26 @@
+import os
+import time
+import pyautogui as pc
+
+end = 0
+
+
+def clear():
+    global end
+    coord = pc.locateOnScreen('src/target.png', confidence=0.7)
+    if coord:
+            end = 0
+            pc.rightClick(coord[0:2])
+            with pc.hold('shift'):
+                return pc.click(pc.locateCenterOnScreen('src/delete.png', grayscale=True))
+    pc.press('pageup')
+    end += 1
+
+
+os.startfile(r"C:\Users\Artyo\OneDrive\Рабочий стол\Discord")
+
+while end < 10:
+    time.sleep(1)
+    clear()
+else:
+    print('•> СООБЩЕНИЯ ОЧИЩЕНЫ ✅')
